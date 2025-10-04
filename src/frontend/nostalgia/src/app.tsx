ae3ef328c869977b327a4240a365dd9b26d45d62
Eti-ini Effiong-Robert <etinieffiongrobert@gmail.com>
2025-10-04 13:12:44 +0100

    Initial commit
    - Set up Django, React, and starter libraries/packages

---
+++ src/frontend/nostalgia/src/app.tsx
@@ -0,0 +1,30 @@
+import "@/app.css";
+import { Home } from "@/app/home";
+import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="*" element={<Navigate to="/home" replace />} />
+      <Route path="home" element={<Home />} />
+    </Routes>
+  );
+};
+
+export const App = () => {
+  const client = new QueryClient();
+
+  return (
+    <QueryClientProvider client={client}>
+      <TooltipProvider>
+        <Toaster position="top-right" theme="dark" />
+        <BrowserRouter>
+          <AppRoutes />
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
---
+++ src/frontend/nostalgia/src/app/home.tsx
@@ -0,0 +1,3 @@
+export const Home = () => {
+  return <h1>Hello, Eti-ini!</h1>;
+};
